3af098d9b1c3c47957cd46e7178cd517f0c0c90e
Maddie <maddiemcampbell@gmail.com>
2019-11-07 14:47:04 -0800

    server responds with get requests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
+app.use(morgan('dev'));
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+const cacheMovieData = {};
+
+app.get('/', function(req, res){
+    if (!cacheMovieData[req.url]){
+        axios.get('http://www.omdbapi.com' + req.url + '&apikey=8730e0e')
+        .then(response =>  {
+            cacheMovieData[req.url] = response.data;
+            res.send(response.data);
+            console.log(JSON.stringify(cacheMovieData))
+        })
+        .catch(error => res.send(error.message)); 
+    }
+    else {
+        res.send(cacheMovieData[req.url]);
+    }
+})
+
+app.get('*', function(req, res){
+    res.status(404).send('Page Not Found');
+})
 
 module.exports = app;
